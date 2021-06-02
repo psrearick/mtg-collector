2dc1e8aa7328f54e4aac1037819eb25a2cf6692a
philliprearick <psrearick@gmail.com>
2021-06-01 20:04:03 -0400

    init

---
+++ app/Actions/DownloadFileAction.php
@@ -0,0 +1,130 @@
+<?php
+
+
+namespace App\Actions;
+
+
+use Carbon\Carbon;
+
+class DownloadFileAction
+{
+    public function __construct()
+    {}
+
+    public function execute(array $file, string $date_format = '', int $clean = 0) : string
+    {
+        // get file name
+        $file_name = $file['name'] ?: $this->getFileName($file, $date_format);
+
+        // get download directory
+        $dir = $this->getFileDirectory($file['storage_path']);
+
+        // Save file into file location
+        $save_file_loc = $dir . '/' . $file_name;
+        $this->saveFile($save_file_loc, $file['url']);
+
+        // remove old files
+        $this->clean($clean, $dir);
+
+        return $save_file_loc;
+    }
+
+    private function saveFile(string $path, string $url) : void
+    {
+        // Only create the file if it doesn't exist
+        if (file_exists($path)) {
+            return;
+        }
+
+        // Initialize the cURL session and open file
+        $ch = curl_init($url);
+        $fp = fopen($path, 'c');
+
+        // set an option for a cURL transfer
+        curl_setopt($ch, CURLOPT_FILE, $fp);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+
+        // Perform a cURL session
+        curl_exec($ch);
+
+        // Closes a cURL session and file and free all resources
+        curl_close($ch);
+        fclose($fp);
+    }
+
+    /**
+     * Build the name of the file
+     *
+     * @param array $file
+     * @param string $date_format
+     * @return string
+     */
+    private function getFileName(array $file, string $date_format) : string
+    {
+        $date = '';
+        if ($date_format) {
+            $date = '_' . Carbon::now()->format($date_format);
+        }
+
+        // return the base name of file
+        return basename($file['url'], '.' . $file['format']) . $date . '.' . $file['format'];
+    }
+
+    /**
+     * get storage directory, make it if needed
+     *
+     * @param string $path
+     * @return string
+     */
+    private function getFileDirectory(string $path) : string
+    {
+        $dir = storage_path($path);
+
+        if (!is_dir($dir)) {
+            mkdir($dir);
+        }
+
+        return $dir;
+    }
+
+    /**
+     * Remove old files
+     *
+     * @param int $max
+     * @param string $dir
+     */
+    private function clean(int $max, string $dir) : void
+    {
+        if (!($dir && $max) || $max < 1) {
+            return;
+        }
+
+        // get files
+        $files = $this->getFilesInDirectory($dir);
+
+        // remove old files
+        $count = 1;
+        foreach ($files as $file => $time){
+            if($count > $max){
+                unlink($file);
+            }
+            $count++;
+        }
+    }
+
+    private function getFilesInDirectory(string $dir) : array
+    {
+        //get all the files
+        $files = array();
+        foreach(scandir($dir) as $file){
+            if(is_file($file)) {
+                $files[$file] = filemtime($file);
+            }
+        }
+
+        //sort descending by date created
+        arsort($files);
+
+        return $files;
+    }
+}
---
+++ app/Jobs/ImportCardData.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Actions\DownloadFileAction;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use pcrov\JsonReader\JsonReader;
+use RecursiveArrayIterator;
+use RecursiveIteratorIterator;
+
+class ImportCardData implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        echo 'running';
+
+        // Pricing data URL
+        $file = [
+            'url'           => "https://mtgjson.com/api/v5/AllPrices.json",
+            'format'        => 'json',
+            'storage_path'  => 'dumps/pricing',
+        ];
+
+        $save_file_loc = (new DownloadFileAction())->execute($file, 'Ymd', 5);
+
+//        // open pricing json file and get prices
+//        $reader = new JsonReader();
+//        $reader->open($save_file_loc);
+//        $reader->read();
+//        $reader->read('data');
+//        $reader->read();
+//        // loop through cards
+//        while($reader->type() === JsonReader::OBJECT) {
+//            $uuid = $reader->name();
+//            $normal = 0;
+//            $foil = 0;
+//            $data = $reader->value();
+//
+//            $tcg = $this->recursiveFind($data, 'tcgplayer');
+//            if (!is_null($tcg)){
+//                if (array_key_exists('retail', $tcg)){
+//                    $retail = $tcg['retail'];
+//                    if (array_key_exists('normal', $retail)){
+//                        $normal = end($retail['normal']);
+//                    }
+//                    if (array_key_exists('foil', $retail)) {
+//                        $foil = end($retail['foil']);
+//                    }
+//                }
+//            }
+//
+//            if ($uuid && $tcg) {
+//                // update prices
+//                CardPrices::updateOrCreate(
+//                    ['uuid' => $uuid],
+//                    ['foil' => $foil, 'normal' => $normal]
+//                );
+//            }
+//
+//            $reader->next();
+//        }
+//
+//        $reader->close();
+//
+//        return 0;
+    }
+
+    private function recursiveFind(array $haystack, string $needle)
+    {
+        $iterator  = new RecursiveArrayIterator($haystack);
+        $recursive = new RecursiveIteratorIterator(
+            $iterator,
+            RecursiveIteratorIterator::SELF_FIRST
+        );
+
+        foreach ($recursive as $key => $value) {
+            if ($key === $needle) {
+                return $value;
+            }
+        }
+
+        return '';
+    }
+}
