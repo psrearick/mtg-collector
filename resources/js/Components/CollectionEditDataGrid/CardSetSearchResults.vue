21655da7b898c8b3a87e34e0bffe8b097d5e64fe
philliprearick <psrearick@gmail.com>
2021-06-14 15:48:27 -0400

    update collection edit page

--- resources/js/Components/CollectionEditDataGrid/CardSetSearchResults.vue
+++ resources/js/Components/CollectionEditDataGrid/CardSetSearchResults.vue
@@ -2,16 +2,16 @@
     <div class="w-full">
         <div
             v-if="cards.length || sets.length"
-            class="rounded-md mt-4 p-4 bg-gray-500"
+            class="rounded-md mt-4 p-4 bg-gray-200"
         >
             <div v-if="cards.length" class="p-4">
-                <button
+                <div
                     v-for="(card, index) in cards"
                     :key="index"
                     class="
-                        block
                         flex
-                        bg-gray-100
+                        justify-between
+                        bg-white
                         rounded-md
                         w-full
                         py-2
@@ -19,9 +19,120 @@
                         mb-2
                     "
                 >
-                    <img :src="card.image_url" :alt="card.name" class="h-48 px-4" />
-                    <span> {{ card.name }} - {{ card.set.name }} <br /> {{ card.feature }} </span>
-                </button>
+                    <img
+                        :src="card.image_url"
+                        :alt="card.name"
+                        class="h-48 px-4"
+                    />
+                    <div class="flex flex-col justify-between">
+                        <div class="text-center">
+                            <p>{{ card.name }} - {{ card.set.name }}</p>
+                            <p class="text-sm text-gray-500">
+                                {{ card.feature }}
+                            </p>
+                        </div>
+                        <div class="flex justify-center py-4">
+                            <div
+                                v-if="card.hasNonFoil"
+                                class="p-2 mx-4 shadow-md bg-gray-100 w-32"
+                            >
+                                <button
+                                    class="
+                                        p-2
+                                        bg-green-500
+                                        hover:bg-green-900
+                                        text-white
+                                        w-full
+                                    "
+                                >
+                                    +
+                                </button>
+                                <p class="text-2xl text-center bg-white w-full">
+                                    0
+                                </p>
+                                <button
+                                    class="
+                                        p-2
+                                        bg-blue-500
+                                        hover:bg-blue-900
+                                        text-white
+                                        w-full
+                                    "
+                                >
+                                    -
+                                </button>
+                                <p
+                                    class="
+                                        w-full
+                                        text-center
+                                        capitalize
+                                        text-gray-700
+                                    "
+                                >
+                                    Non-Foil
+                                </p>
+                            </div>
+                            <div
+                                v-if="card.hasFoil"
+                                class="p-2 mx-4 shadow-md bg-gray-100 w-32"
+                            >
+                                <button
+                                    class="
+                                        p-2
+                                        bg-green-500
+                                        hover:bg-green-900
+                                        text-white
+                                        w-full
+                                    "
+                                >
+                                    +
+                                </button>
+                                <p class="text-2xl text-center bg-white w-full">
+                                    0
+                                </p>
+                                <button
+                                    class="
+                                        p-2
+                                        bg-blue-500
+                                        hover:bg-blue-900
+                                        text-white
+                                        w-full
+                                    "
+                                >
+                                    -
+                                </button>
+                                <p
+                                    class="
+                                        w-full
+                                        text-center
+                                        capitalize
+                                        text-gray-700
+                                    "
+                                >
+                                    Foil
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <p class="text-right">
+                            <span class="text-sm text-gray-500 mr-2"
+                                >Non-Foil</span
+                            >
+                            <span v-if="card.hasNonFoil">{{
+                                format(card.price_normal)
+                            }}</span>
+                            <span v-else>N/A</span>
+                        </p>
+                        <p class="text-right">
+                            <span class="text-sm text-gray-500 mr-2">Foil</span>
+                            <span v-if="card.hasFoil">{{
+                                format(card.price_foil)
+                            }}</span>
+                            <span v-else>N/A</span>
+                        </p>
+                    </div>
+                </div>
             </div>
             <div v-if="!cards.length" class="p-4">
                 <button
@@ -37,6 +148,8 @@
 </template>
 
 <script>
+import formatCurrency from "@/Shared/api/ConvertValue";
+
 export default {
     name: "CardSetSearchResults",
 
@@ -48,6 +161,12 @@ export default {
         sets: {
             type: Array,
             default: () => {},
+        },
+    },
+
+    methods: {
+        format(value) {
+            return value ? formatCurrency(value) : "N/A";
         },
     },
 };
