9ab89903b70f045c31ed722e66ebaa7efac8fa86
Phillip Rearick <psrearick@gmail.com>
2021-12-22 08:45:19 -0500

    Webppack fixes

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,25 @@
-const path = require('path');
+const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                enforce: "pre",
+                use: ["source-map-loader"],
+            },
+        ],
+    },
     resolve: {
         alias: {
-            '@': path.resolve('resources/js'),
+            "@": path.resolve("resources/js"),
         },
     },
+    plugins: [
+        new webpack.DefinePlugin({
+            __VUE_OPTIONS_API__: true,
+            __VUE_PROD_DEVTOOLS__: false,
+        }),
+    ],
 };
